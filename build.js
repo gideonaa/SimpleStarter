ee54d90f919c904c0a6bdd2e63ddb0556ceec11f
Gideon Arom <gideonarom@Gideons-MBP.attlocal.net>
2022-03-03 09:08:06 -0800

    working draft of build that accepts json content

--- build.js
+++ build.js
@@ -1,33 +1,39 @@
 const nunjucks = require('nunjucks')
 const fs = require('fs');
+const path = require('path');
 
 // tell nunjucks where to find templates
-var env = nunjucks.configure(['./','/src/templates/','/src/pages/']);
-var extension = '.html' // extension of file output. Set empty quotes for none.
+var env = nunjucks.configure(['./','/src/templates/','/src/pages/'], { autoescape: false });
+// Set extension of compiled output file. Use empty quotes for no extension.
+var extension = '.html'
 
-// Loop through all the files in the temp directory
+// Loop through all the files and render them
 fs.readdir('src/pages/', function (err, files) {
+  // handle errors reading dir
   if (err) {
     console.error("Could not list the directory.", err);
     process.exit(1);
   }
-  files.forEach(function (file, index) {
-    var inputfile = env.getTemplate('src/pages/' + file);
-    var output = inputfile.render();
-    console.log('Building file '+file+'...');
-    fs.writeFileSync(__dirname +'/public/'+ file.replace('.njk', extension), output);
+  files.forEach(file => {
+    //var filepath = 'src/pages/'+file;
+    if (path.extname(`src/pages/${file}`) == ".njk"){
+      // get the input njk file to be compiled
+      console.log(`**Building file ${file}`)
+      var inputfile = env.getTemplate(`src/pages/${file}`);
+      // get associated json data file, if applicable
+      var datafile;
+      try {
+        json = `src/pages/`+file.replace('.njk', '.json');
+        data = fs.readFileSync(json,'utf8');
+      } catch (err) {
+        console.log(`No JSON content file associated with ${file}`)
+        data = null;
+      }
+      // render the page/template/input data into a var
+      var outputfile = inputfile.render(JSON.parse(data));
+      // write the data to the file system
+      // TODO make work for non .njk files
+      fs.writeFileSync(`${__dirname}/public/${file.replace('.njk', extension)}`, outputfile);
+    }
   });
 });
-
-
-
-
-//
-//
-// try {
-//   const files = fs.readdir('/src/pages/', renderFiles(err, files));
-//   for (const file of files)
-//     console.log(file);
-// } catch (err) {
-//   console.error(err);
-// }
